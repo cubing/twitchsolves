e90e0b717b54de4bcd69b8a4132fed032087de46
Lucas Garron <code@garron.net>
2020-05-30 15:05:48 -0700

    Remove vendored `/cubing.js` folder.

--- cubing.js/src/cubing/no-twisty.ts
+++
@@ -1,8 +0,0 @@
-// TODO: `export * as ...`?
-import * as alg from "../alg";
-import * as bluetooth from "../bluetooth";
-import * as kpuzzle from "../kpuzzle";
-
-export { alg as alg };
-export { kpuzzle as kpuzzle };
-export { bluetooth as bluetooth };
